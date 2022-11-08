9ec675f97a6f395cc3b7ae88957b1200985f1fcb
Thomas Fogarty <t.fogarty24@outlook.com>
2022-11-08 16:25:31 +0000

    refactoring code to use constructors

--- src/to-dos/to-do.js
+++ src/to-dos/to-do.js
@@ -1,23 +1,5 @@
 
-function CreateToDo(title, description, dueDate, priority) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority
-};
-
-const newToDo = new CreateToDo("Shopping", "Groceries", "Tuesday", "Urgent");
 
 
 
-function loadToDo() {
-    const container = document.getElementById('container');
-    const toDoContainer = document.createElement('div');
 
-    toDoContainer.textContent = newToDo.title + " " + newToDo.description + " " + newToDo.dueDate + " " + newToDo.priority;
-
-    container.appendChild(toDoContainer)
-    
-}
-
-export default loadToDo;
