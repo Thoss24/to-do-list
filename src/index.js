d1ebb5bc22ed94d87936c60914c81e77365b6e7d
Thomas Fogarty <t.fogarty24@outlook.com>
2022-11-28 17:12:44 +0000

    testing options for storing projects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import Projects from "./project";
 
+
 const projectName = document.getElementById('project-name');
 const projectNameButton = document.getElementById('project-name-button')
+
+
 
 projectNameButton.addEventListener('click', () => {
     const userInputTitle = projectName.value
 
     const newProject = new Projects(userInputTitle)
     newProject.checkName()
+
+    newProject.projects.push(newProject)
+    newProject.test()
 })
 
 
-
+// Figure out how to store projects in a seperate folder
 
 
 
--- src/project.js
+++ src/project.js
@@ -2,10 +2,18 @@ class Projects {
     constructor(title) {
         this.title = title;
         this.toDo = [];
+        this.projects = []
     }
 
     checkName() {
         console.log(this.title)
+        
+    }
+
+    test() {
+        for (let i = 0; i < this.projects.length; i++) {
+            console.log(i)
+        }
     }
 }
 
