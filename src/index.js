a21ba99b514a1f8c9ac269a26534a6246a0bd490
Thomas Fogarty <t.fogarty24@outlook.com>
2022-12-10 15:12:56 +0000

    basic todo function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 
+//Selectors
+const toDoInput = document.querySelector('.todo-input');
+const toDoButton = document.querySelector('.todo-button');
+const toDoList = document.querySelector('.todo-list');
 
+//Event listeners
+toDoButton.addEventListener('click', addTodo)
 
+//Functions
+function addTodo(event) {
+    event.preventDefault();
+    // ToDo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Checked button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+     // Trash button
+     const trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class="fas fa-check"></i>';
+     trashButton.classList.add('complete-btn');
+     todoDiv.appendChild(trashButton);
+}
 
 
 
