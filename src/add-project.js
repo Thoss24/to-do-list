4377bb348d9c779ec6c4e04445b8186b85311764
Thomas Fogarty <t.fogarty24@outlook.com>
2022-12-13 17:40:05 +0000

    styling added to todos

--- src/add-project.js
+++ src/add-project.js
@@ -4,5 +4,11 @@ const projectButton = document.querySelector('project-button');
 const projectList = document.querySelector('project-list');
 
 // Event listeners
+projectButton.addEventListener('click', addProject);
 
 // Functions
+function addProject(e) {
+    e.preventDefault();
+    // This Project's Todo's
+    const projectToDos = [];
+}
