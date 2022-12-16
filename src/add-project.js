b09c856c18847e4779d099630542b7a57354a886
Thomas Fogarty <t.fogarty24@outlook.com>
2022-12-16 17:10:27 +0000

    comment added

--- src/add-project.js
+++ src/add-project.js
@@ -4,29 +4,33 @@ const projectButton = document.querySelector('project-button');
 const projectList = document.querySelector('project-list');
 
 // Event listeners
-projectButton.addEventListener('click', createProject);
+projectButton.addEventListener('click', () => {
+    console.log("hello") 
+});
 
-// Functions
-function createProject(event) {
-    // prevent page refresh
-    event.preventDefault();
-    // Array to store project to-dos
-    const projectsToDos = [];
-    // Create project icon
-    const projectDiv = document.createElement('div');
-    projectDiv.classList.add('project-div');
-    // New Project
-    const newProject = document.createElement('li');
-    newProject.innerText = "test project";
-    newProject.classList.add('new-project');
-    projectDiv.appendChild(newProject);
-     // Trash button
-     const trashButton = document.createElement('button');
-     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-     trashButton.classList.add('trash-btn');
-     projectDiv.appendChild(trashButton);
-     // Append to list
-     projectList.appendChild(projectDiv);
-}
+// figure out why I am getting TypeError: Cannot read properties of null (reading 'addEventListener')
+
+// // Functions
+// function createProject(event) {
+//     // prevent page refresh
+//     event.preventDefault();
+//     // Array to store project to-dos
+//     const projectsToDos = [];
+//     // Create project icon
+//     const projectDiv = document.createElement('div');
+//     projectDiv.classList.add('project-div');
+//     // New Project
+//     const newProject = document.createElement('li');
+//     newProject.innerText = "test project";
+//     newProject.classList.add('new-project');
+//     projectDiv.appendChild(newProject);
+//      // Trash button
+//      const trashButton = document.createElement('button');
+//      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+//      trashButton.classList.add('trash-btn');
+//      projectDiv.appendChild(trashButton);
+//      // Append to list
+//      projectList.appendChild(projectDiv);
+// }
 
 export {createProject, projectButton}
