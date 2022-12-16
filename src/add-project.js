571fe65b650b632ab4c512401df62d6851f0e0e0
Thomas Fogarty <t.fogarty24@outlook.com>
2022-12-16 17:32:54 +0000

    ui of project now showing

--- src/add-project.js
+++ src/add-project.js
@@ -1,36 +1,33 @@
 // Selectors
-const projectInput = document.querySelector('project-input');
-const projectButton = document.querySelector('project-button');
-const projectList = document.querySelector('project-list');
+const projectInput = document.querySelector('.project-input');
+const projectButton = document.querySelector('.project-button');
+const projectList = document.querySelector('.project-list');
 
 // Event listeners
-projectButton.addEventListener('click', () => {
-    console.log("hello") 
-});
+projectButton.addEventListener('click', createProject);
 
-// figure out why I am getting TypeError: Cannot read properties of null (reading 'addEventListener')
-
-// // Functions
-// function createProject(event) {
-//     // prevent page refresh
-//     event.preventDefault();
-//     // Array to store project to-dos
-//     const projectsToDos = [];
-//     // Create project icon
-//     const projectDiv = document.createElement('div');
-//     projectDiv.classList.add('project-div');
-//     // New Project
-//     const newProject = document.createElement('li');
-//     newProject.innerText = "test project";
-//     newProject.classList.add('new-project');
-//     projectDiv.appendChild(newProject);
-//      // Trash button
-//      const trashButton = document.createElement('button');
-//      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-//      trashButton.classList.add('trash-btn');
-//      projectDiv.appendChild(trashButton);
-//      // Append to list
-//      projectList.appendChild(projectDiv);
-// }
+// Functions
+function createProject(event) {
+    // prevent page refresh
+    event.preventDefault();
+    // Array to store project to-dos
+    const projectsToDos = [];
+    // Create project icon
+    const projectDiv = document.createElement('div');
+    projectDiv.classList.add('project-div');
+    // New Project
+    const newProject = document.createElement('li');
+    newProject.innerText = "test project";
+    newProject.classList.add('new-project');
+    projectDiv.appendChild(newProject);
+     // Trash button
+     const trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+     trashButton.classList.add('trash-btn');
+     projectDiv.appendChild(trashButton);
+     // Append to list
+     projectList.appendChild(projectDiv);
+}
 
 export {createProject, projectButton}
+
--- src/index.js
+++ src/index.js
@@ -5,4 +5,3 @@ addTodo();
 createProject()
 
 
-console.log(projectButton)
