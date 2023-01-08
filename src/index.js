d0d5fc1ecbe314be216a151afed6697f6013df35
Thomas Fogarty <t.fogarty24@outlook.com>
2023-01-08 20:54:01 +0000

    innerHTML content added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {CreateProject} from "./add-project";
 // Projects container
 let myProjects = [];
 
-// Functions
 
 const dom = (() => {
 
@@ -30,20 +29,18 @@ const dom = (() => {
     
         const btn = projectList.getElementsByClassName("project-ui-button");
         projectUiButton.addEventListener('click', () => {
+            
             projectTodoTitle.textContent = newProject.projectName;
            });
     
         // Loop through the buttons and add the active class to the current/clicked button
         for (let i = 0; i < btn.length; i++) {
             btn[i].addEventListener("click", function() {
-            
             let current = document.getElementsByClassName("active");
-    
             // If there's no active class
             if (current.length > 0) {
                 current[0].className = current[0].className.replace(" active", "");
-            }
-    
+            };
             // Add the active class to the current/clicked button
             this.className += " active";
         });
@@ -56,22 +53,25 @@ const dom = (() => {
     function addTodo(e) {
     
        const projectUiButton = document.querySelector('.project-ui-button.active');
-    
        const newTodo = new CreateTodo(toDoInput.value, todoDateInput.value);
+
+    //    (projectUiButton.textContent == myProjects[i].projectName)
     
        for (let i = 0; i < myProjects.length; i++) {
-    
-            if (projectUiButton.textContent == myProjects[i].projectName) { // add condition so that 2 projects cannot have the same name
-             //   const newTodoValues = [newTodo.title, newTodo.date, newTodo.checked]
-    
-                myProjects[i].tasks.push(newTodo);
+            if (projectUiButton.classList.contains('active')) { // add condition so that 2 projects cannot have the same name
+                const newTodoValues = [newTodo.title, newTodo.date, newTodo.completed]
+                myProjects[i].tasks.push(newTodoValues);
+                const todoItems = document.createElement('div');
+                const checked = document.createElement('input');
+                checked.type = "checkbox";
+                todoItems.classList.add('todo-items');
+                const todoTitle = myProjects[i].tasks[0][0];
+                const todoDate = myProjects[i].tasks[0][1];
+                todoItems.append(todoTitle, todoDate);
+                toDoContainer.innerHTML = 
+                    '<div class="todo-items"> <input type="checkbox"> </div>' // inner HTML is the key, continue to test this for solution
                 console.log(myProjects[i].tasks)
-    
-                const todoItemContainer = document.createElement('button');
-                todoItemContainer.classList.add('todo-item-container');
-    
-                toDoContainer.append(todoItemContainer)
-            }; 
+            }
        };
     };
 
