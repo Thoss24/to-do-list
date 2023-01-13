f0d6e5775ceeeca5a29ef17054863a1cec55dd75
Thomas Fogarty <t.fogarty24@outlook.com>
2023-01-13 18:25:34 +0000

    Dom class added with method to display project input

--- src/dom.js
+++ src/dom.js
@@ -1,23 +1,26 @@
 import Todo from "./add-todo";
 import Projects from "./add-project";
 
-
 export default class Dom {
 
-static loadPage() {
-    Dom.addProjectUi()
-}
+    static addProjectUi() {
+        const addProjectContainer = document.getElementById('add-project-container');
+        const addProjectButton = document.getElementById('add-projects-button');
+        const cancelAddProjectButton = document.getElementById('cancel-add-project-button');
+        const createProjectButton = document.getElementById('create-project-button');
 
+        addProjectButton.addEventListener('click', () => {
+            addProjectContainer.style.display = "flex"
+        });
 
-static addProjectUi() {
+        cancelAddProjectButton.addEventListener('click', (event) => {
+            event.preventDefault()
+            addProjectContainer.style.display = "none"
+        });
 
-    const addProjectButton = document.querySelector('.add-projects-button');
-    const addProjectContainer = document.getElementById('add-project-container');
-
-    addProjectButton.addEventListener('click', () => {
-        addProjectContainer.sty
-    })
-}   
- 
-    
+        createProjectButton.addEventListener('click', (event) => {
+            event.preventDefault()
+        })
+        
+    }
 }
